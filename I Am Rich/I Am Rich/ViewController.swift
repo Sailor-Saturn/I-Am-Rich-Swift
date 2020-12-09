b1a955b893fdfe02a83481a0220ed5610fdc4abb
Vera Dias <veradias97@hotmail.com>
2020-12-09 14:50:47 +0000

    add submodule

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Vera Dias on 07/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
